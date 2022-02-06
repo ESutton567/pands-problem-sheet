d2680242c7228b6bee018dd1e1b9f355a418fb72
Eilis Sutton <G00411335@gmit.ie>
2022-02-06 21:23:07 +0000

    pands-problem-sheet

--- secondstring.py
+++ secondstring.py
@@ -1,7 +1,8 @@
 # This program asks the user to input a string and outputs every second letter in reverse order
 # Author: Éilis Sutton
 
-# To retrieve every other character of the input sentance from index 0 (0 being the first character) to the end -> [::2] 
+# To retrieve every other character of the input sentance from index 0 (0 being the first character)
+# to the end add [::2] following the input message instruction
 # Ref: https://stackoverflow.com/questions/20847205/program-to-extract-every-alternate-letters-from-a-string-in-python
 
 # To retrieve these characters in reverse order add a minus in to [::2] -> [::-2] 
