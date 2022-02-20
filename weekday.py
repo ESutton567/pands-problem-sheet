1a8d834cff55bcc143edd0d400dc2923ef84bf8f
Eilis Sutton <G00411335@gmit.ie>
2022-02-20 22:04:13 +0000

    pands-problem sheet

---
+++ weekday.py
@@ -0,0 +1,13 @@
+# This program outputs whether or not today is a weekday
+# Éilis Sutton
+# Ref 1: https://www.w3schools.com/python/python_datetime.asp
+# Ref 2: https://www.tutorialsrack.com/articles/324/how-to-find-the-current-day-is-weekday-or-weekends-in-python
+
+import datetime             # This imports the 'datetime' module
+
+weekNumber = datetime.datetime.today().weekday()  # Gets the week number
+
+if weekNumber < 5:
+    print("Yes, unfortunately today is a weekday.")
+else: 
+    print("It is the weekend")
