6c3ff530139019a239c22712cc985de9eb3ba189
Eilis Sutton <G00411335@gmit.ie>
2022-03-26 17:38:27 +0000

    Add comments to explain program function

--- bmi.py
+++ bmi.py
@@ -7,7 +7,11 @@
 # Height must be input as cm as decimal point numbers (kg value) cannot be computed to base 10 (Figured this out through an error I received)
 # Height must also be converted to kg in the program as the formula requires it in this format
 
+# asks the user to input their weight
 weight = int(input("Please enter your weight (kg):"))
+# asks the user to input their height
 height = int(input("Please enter your height (cm):"))
+# creates a variable that calculates BMI as per the formula
 newNumber = weight/((height/100)**2)
+# outputs the variable based on the user input
 print('The BMI is kg/m2: {}' .format (newNumber))
